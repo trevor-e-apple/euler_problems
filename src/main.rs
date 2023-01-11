70fa119ce1ab56b07e1621bd96d1bcd73953ebd2
Trevor Apple <trevorapple@Trevors-MacBook-Air.local>
2023-01-10 18:47:24 -0800

    added benchmarks and stuff for euler problems

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+#![feature(test)]
+extern crate test;
+
 use crate::problem_one::{
     problem_one_asm,
-    problem_one_closed_form_plain,
+    problem_one_closed_form,
     problem_one_plain
 };
 
@@ -8,6 +11,6 @@ mod problem_one;
 
 fn main() {
     println!("Problem one solution: {}", problem_one_plain(1000));
-    println!("Problem one solution: {}", problem_one_closed_form_plain(1000));
+    println!("Problem one solution: {}", problem_one_closed_form(1000));
     println!("Problem one solution: {}", problem_one_asm(1000));
 }
--- src/problem_one.rs
+++ src/problem_one.rs
@@ -4,6 +4,7 @@ fn triangle_number(n: i32) -> i32 {
     (n * (n + 1)) / 2
 }
 
+#[inline(never)]
 pub fn problem_one_plain(n: i32) -> i32 {
     let mut sum = 0;
     for i in 0..n {
@@ -15,7 +16,8 @@ pub fn problem_one_plain(n: i32) -> i32 {
     sum
 }
 
-pub fn problem_one_closed_form_plain(n: i32) -> i32 {
+#[inline(never)]
+pub fn problem_one_closed_form(n: i32) -> i32 {
     // the problem defines the sum of all numbers less than n
     let n = n - 1;
 
@@ -25,26 +27,46 @@ pub fn problem_one_closed_form_plain(n: i32) -> i32 {
     multiples_of_three + multiples_of_five - multiples_of_fifteen
 }
 
-pub fn triangle_number_asm(n: i32) -> i32 {
-    let mut result: i32 = n;
+fn sum_of_multiples_asm(n: u32, divisor: u32) -> u32 {
+    let mut result: u32 = n;
+    let divide_mul: u32 = divisor;
     unsafe {
         asm!(
+            "udiv {result:x}, {result:x}, {divide_mul:x}",
             "mov {tmp}, {result:x}",
             "mul {result:x}, {result:x}, {result:x}",
             "add {result:x}, {result:x}, {tmp}",
             "asr {result:x}, {result:x}, 1",
-            result = inout(reg) result,
+            "mul {result:x}, {result:x}, {divide_mul:x}",
+            divide_mul = in(reg) divide_mul,
+            result = inlateout(reg) result,
             tmp = out(reg) _,
         );
     }
     result
 }
 
-pub fn problem_one_asm(n: i32) -> i32 {
+#[inline(never)]
+pub fn problem_one_asm(n: u32) -> u32 {
     let n = n - 1;
 
-    let multiples_of_three = 3 * triangle_number_asm(n / 3);
-    let multiples_of_five = 5 * triangle_number_asm(n / 5);
-    let multiples_of_fifteen = 15 * triangle_number_asm(n / 15);
+    let multiples_of_three = sum_of_multiples_asm(n, 3);
+    let multiples_of_five = sum_of_multiples_asm(n, 5);
+    let multiples_of_fifteen = sum_of_multiples_asm(n, 15);
     multiples_of_three + multiples_of_five - multiples_of_fifteen
+}
+
+#[bench]
+fn plain(b: &mut test::Bencher) {
+    b.iter(|| problem_one_plain(1000))
+}
+
+#[bench]
+fn closed(b: &mut test::Bencher) {
+    b.iter(|| problem_one_closed_form(1000))
+}
+
+#[bench]
+fn asm(b: &mut test::Bencher) {
+    b.iter(|| problem_one_asm(1000))
 }
